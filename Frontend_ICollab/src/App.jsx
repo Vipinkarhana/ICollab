00cb5f68f898041d5f38dd5fb4a13b2e3ac87f04
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-21 13:06:31 +0530

    Frontend Initiated and Tailwind set

---
+++ Frontend_ICollab/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-green-400 text-green-800 text-4xl'>Tailwind Initiated</h1>
+  )
+}
+
+export default App
