b83b9fb53e4907786a25aeb42650e62e6e4eeb52
Naman065Malik <naman065op@gmail.com>
2025-01-23 02:33:01 +0530

    Boiler Plate Updated

--- Backend_ICollab/app.js
+++ Backend_ICollab/app.js
@@ -1,17 +1,12 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var ApiError = require('./utils/ApiError');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,22 +15,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  next(new ApiError(404, "The requested resource couldn't be found"));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Global Error-Handling Middleware
+const errorHandler = (err, req, res, next) => {
+  console.error(`Error: ${err.message}`);
+  res.status(err.statusCode || 500).json({
+    error: {
+      message: err.message || 'Internal Server Error',
+      stack: process.env.NODE_ENV === 'production' ? ' ' : err.stack,
+    },
+    status: 'failed',
+  });
+};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Use the error-handling middleware
+app.use(errorHandler);
 
 module.exports = app;
