487af60cd84de6048d89361ba1633c9e137a5de1
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-02-15 13:29:19 +0530

    little changes

--- Frontend_ICollab/src/components/Pages/ProfilePage/ProfileCard/ProfileCard.jsx
+++ Frontend_ICollab/src/components/Pages/ProfilePage/ProfileCard/ProfileCard.jsx
@@ -10,7 +10,7 @@ function ProfileCard() {
   const [isOpen, SetIsOpen] = useState(false);
   return (
     <div className="w-[100%] h-24  flex justify-between bg-gray-200 rounded-md py-2 border border-gray-400 text-gray-800">
-      <div className="w-[43%] h-full  flex justify-evenly items-center p-2">
+      <div className="w-auto h-full  flex justify-evenly items-center p-2">
         <ProfilePic picture={user?.profile_pic} className="h-20 w-20" />
         <Name_Designation name={user?.name} designation={user?.designation} nameClass="text-2xl" designationClass="text-lg" />
       </div>
