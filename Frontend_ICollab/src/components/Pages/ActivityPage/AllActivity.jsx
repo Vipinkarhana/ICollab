0da1a3bf0f33c41578e2058f899042c85ba80caf
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-02-15 19:18:49 +0530

    Minor changes

--- Frontend_ICollab/src/components/Pages/ActivityPage/AllActivity.jsx
+++ Frontend_ICollab/src/components/Pages/ActivityPage/AllActivity.jsx
@@ -3,30 +3,34 @@ import PostCard from "../HomePage/MidDiv/Feed/Posts/Postcard/PostCard";
 import media from "/public/ProfilePic.png";
 function AllActivity() {
   const posts = [
-    {
-      text: "This is a post 1",
-      media: media,
-    },
-    {
-      text: "This is a post 2",
-      media: media,
-    },
-    {
-      text: "This is a post 3",
-      media: media,
-    },
+    // {
+    //   text: "This is a post 1",
+    //   media: media,
+    // },
+    // {
+    //   text: "This is a post 2",
+    //   media: media,
+    // },
+    // {
+    //   text: "This is a post 3",
+    //   media: media,
+    // },
   ]
   return (
-    <div className="w-auto h-auto flex flex-col justify-start items-center gap-2 ">
-        <div className="h-12 w-[100%] border-b border-gray-400 flex items-center px-4 text-2xl font-semibold">
-            <p>All Activity</p>
+    <div className="w-auto h-auto flex flex-col justify-start items-center ">
+      <div className="h-12 w-[100%] border-b border-gray-400 flex items-center px-4 text-2xl font-semibold">
+        <p>All Activity</p>
       </div>
-      <div className="h-auto w-[98%] flex flex-col justify-start items-center p-2 gap-1 ">
-        {
-          posts.map((post) => {
-            return <PostCard text={post.text} media={post.media} />;
-          })
-        }
+      <div className="h-auto w-[98%] flex flex-col justify-start items-center p-2 gap-1">
+        {posts.length > 0 ? (
+          posts.map((post, index) => (
+            <PostCard key={index} text={post.text} media={post.media} />
+          ))
+        ) : (
+          <div className="text-gray-500 text-lg font-medium mt-4 h-[70svh]">
+            No Posts Yet
+          </div>
+        )}
       </div>
     </div>
   );
