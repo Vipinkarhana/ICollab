c22d1759c33b984b150c41bcbed317a5a4b87c76
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2025-02-19 00:13:38 +0530

    added admin comp, routes and controller

--- Backend_ICollab/src/controllers/adminController.js
+++ Backend_ICollab/src/controllers/adminController.js
@@ -27,28 +27,4 @@ const isAdmin = async (req, res, next) => {
   }
 };
 
-
-
-
-const createAdmin = async (req, res, next) => {
-  try {
-    const username = req.user.username;
-    const user = await userModel.findOne({ username: username });
-
-    const newAdmin = new adminModel({
-      user: user._id,
-    });
-
-    await newAdmin.save();
-
-    res.status(201).json({
-      message: 'Admin created successfully',
-      data: { adminid: newAdmin._id },
-      status: 'success',
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-module.exports = { createAdmin, isAdmin };
+module.exports = { isAdmin };
--- Backend_ICollab/src/routes/adminRoute.js
+++ Backend_ICollab/src/routes/adminRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { isAdmin, createAdmin } = require('../controllers/adminController');
+const { isAdmin } = require('../controllers/adminController');
 const { isloggedin } = require('../middlewares/auth');
 
 const router = express.Router();
