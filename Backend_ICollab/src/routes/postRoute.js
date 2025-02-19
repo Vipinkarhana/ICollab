cf15dac7362e6189183a1ebf4b17ddf648cc8a1e
Ayush Badola <glitchy.56@outlook.com>
2025-02-19 15:45:41 +0530

    Adding Edit Backend

--- Backend_ICollab/src/routes/postRoute.js
+++ Backend_ICollab/src/routes/postRoute.js
@@ -9,11 +9,14 @@ const {
 } = require('../controllers/postController');
 const { validatePost } = require('../middlewares/validation');
 const { isloggedin } = require('../middlewares/auth');
+const {editPost, deletePost} = require('../controllers/editController');
 
 router.post('/', isloggedin, addpost);
 router.get('/mypost', isloggedin, getMyPost);
 router.post('/addmedia', isloggedin, addPostMedia);
 router.post('/likepost', isloggedin, likepost);
 router.get('/feed', isloggedin, feed);
+router.post('/editpost', isloggedin, editPost);
+router.get('/deletepost', isloggedin, deletePost);
 
 module.exports = router;
