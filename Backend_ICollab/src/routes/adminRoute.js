e632b277284487beee65b1d8841300850b47e29f
Naman065Malik <naman065op@gmail.com>
2025-02-19 13:47:24 +0530

    Reverted Rudra Commit: d7349b86d61e3dba2010f8f563ed0b73b7dbd5e4

--- Backend_ICollab/src/routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { isAdmin } = require('../controllers/adminController');
-const { isloggedin } = require('../middlewares/auth');
-
-const router = express.Router();
-
-router.get('/', isloggedin, isAdmin);
-
-module.exports = router;
