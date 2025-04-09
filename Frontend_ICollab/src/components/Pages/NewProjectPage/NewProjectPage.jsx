8a8f6ae65f53cfeb4f0c6a03e908ba65ff75cf3f
Naman065Malik <naman065op@gmail.com>
2025-04-09 20:41:57 +0530

    New Project Page Created

---
+++ Frontend_ICollab/src/components/Pages/NewProjectPage/NewProjectPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewProjectPage() {
+  return (
+    <div className='text-2xl font-bold'>
+      New Project Page/ Create New Project Page
+    </div>
+  )
+}
+
+export default NewProjectPage
