e695d5e3d58b53e074023eddb815964338e9915c
Ayush Badola <glitchy.56@outlook.com>
2025-04-24 22:21:58 +0530

    Complemeted Projects route

--- Backend_ICollab/src/models/project.js
+++ Backend_ICollab/src/models/project.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+const technologyPath = path.join(__dirname, '../../config/technologies.json');
+const allowedTechnologies = JSON.parse(fs.readFileSync(technologyPath, 'utf8'));
+
+const typePath = path.join(__dirname, '../../config/types.json');
+const allowedTypes = JSON.parse(fs.readFileSync(typePath, 'utf8'));
 
 let projectschema = mongoose.Schema(
   {
@@ -7,38 +15,63 @@ let projectschema = mongoose.Schema(
           ref: 'user',
           required: true,
     },
-    title: {
+    name: {
+      type: String,
+      required: true,
+    },
+    tagline: {
       type: String,
       required: true,
     },
     description: {
       type: String,
+    },
+    problem: {
+      type: String,
+    },
+    challenges: {
+      type: String,
+    },
+    technology: {
+      type: String,
+      enum: allowedTechnologies,
       required: true,
     },
-    category: {
-        type: String,
-        required: true,
-      },
+    type: {
+      type: String,
+      enum: allowedTypes,
+      required: true,
+    },
+    links: {
+      type: String,
+      required:true,
+    },
+    videoLink: {
+      type: String,
+    },
     startDate: {
-        type: Date,
-        required: true,
-      },
+      type: Date,
+      required: true,
+    },
     endDate: {
         type: Date,
-      },
-    role: {
-        type: String,
-        required: true,
-      },
+    },
+    // role: {
+    //     type: String,
+    //     required: true,
+    // },
     media: {
       type: [String],
     },
-    tags: {
+    logo: {
       type: String,
     },
-    collaborators: {
-        type: String,
-      },
+    // tags: {
+    //   type: String,
+    // },
+    // collaborators: {
+    //     type: String,
+    //   },
     isOngoing: {
         type: Boolean
     }
--- Backend_ICollab/src/routes/projectRoute.js
+++ Backend_ICollab/src/routes/projectRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const {
-  addProject
+  addProject,
+  technologySuggestions
 } = require('../controllers/projectController');
 const { isloggedin } = require('../middlewares/auth');
-
-router.post('/addproject', isloggedin, addProject);
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+router.post('/addproject', isloggedin, upload.fields([{name: 'logo', maxCount: 1}, {name: 'media', maxCount: 5}]) ,addProject);
+router.get('/technologysuggestions', isloggedin, technologySuggestions);
 
 module.exports = router;
