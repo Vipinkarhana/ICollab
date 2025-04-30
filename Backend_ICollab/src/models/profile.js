8948e97c3d4a5d84f09faa9c34c4ccf35c77e4c9
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-04-30 10:05:15 +0530

    Profile model

--- Backend_ICollab/src/models/profile.js
+++ Backend_ICollab/src/models/profile.js
@@ -1,41 +1,59 @@
 const mongoose = require('mongoose');
 
-let profileschma = mongoose.Schema(
-  {
-    social: {
-      linkedin: {
-        type: String,
-      },
-      twitter: {
-        type: String,
-      },
-      github: {
-        type: String,
-      },
-    },
-    about: {
-      type: String,
-    },
-    experience: {
-      company: String,
-      startDate: Date,
-      endDate: Date,
-      role: String,
-      desc: String,
-    },
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
-  {
-    timestamps: true,
-    toJSON: {
-      transform: (doc, ret) => {
-        delete ret._id;
-        delete ret.__v;
-        delete ret.createdAt;
-        delete ret.updatedAt;
-        return ret;
-      },
-    },
-  }
-);
 
-module.exports = mongoose.model('profile', profileschma);
+  address: {
+    state: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    }
+  },
+
+  skills: {
+    type: [String],
+    validate: [arrayLimit, '{PATH} exceeds the limit of 5']
+  },
+
+  stats: {
+    posts: {
+      type: Number,
+      default: 0
+    },
+    projects: {
+      type: Number,
+      default: 0
+    },
+    collaborators: {
+      type: Number,
+      default: 0
+    },
+    savedItems: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  topProjects: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'project'
+  }]
+}, { timestamps: true });
+
+function arrayLimit(val) {
+  return val.length <= 5;
+}
+
+profileSchema.path('topProjects').validate(function (val) {
+  return val.length <= 3;
+}, 'You can only pin up to 3 projects.');
+
+module.exports = mongoose.model('profile', profileSchema);
