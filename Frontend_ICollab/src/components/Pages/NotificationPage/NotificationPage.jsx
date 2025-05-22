8437b874a17f44278480552d848d95df64c7d6da
mohitgoel-2004 <mohitgoel524@gmail.com>
2025-05-22 17:37:03 +0530

    Header responsives to completed

--- Frontend_ICollab/src/components/Pages/NotificationPage/NotificationPage.jsx
+++ Frontend_ICollab/src/components/Pages/NotificationPage/NotificationPage.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+// src/components/Notification/index.jsx or wherever your NotificationDropdown is
 
-const NotificationPage = () => {
+import { Bell } from "lucide-react";
+
+const NotificationDropdown = () => {
+  const notificationCount = 3; // or use props/state
+
   return (
-    <>
-      <div className="text-black w-full h-svh flex justify-center items-center">
-        Notification page
-      </div>
-    </>
-  );
-}
+    <div className="relative flex flex-col items-center text-sm hover:text-black cursor-pointer">
+      {/* Bell Icon */}
+      <Bell size={20} />
 
-export default NotificationPage;
+      {/* Notification count badge */}
+      {notificationCount > 0 && (
+        <span className="absolute -top-1 -right-2 text-xs bg-red-500 text-white rounded-full px-1.5">
+          {notificationCount}
+        </span>
+      )}
+
+      {/* Label text below the icon */}
+      <span className="text-xs mt-1 hidden lg:block">Notifications</span>
+    </div>
+  );
+};
+
+export default NotificationDropdown;
