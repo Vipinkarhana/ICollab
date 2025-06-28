d44754499a2265509e0aa7522f7b653f8903b65c
Tanmay <tanmaysharmats36@gmail.com>
2025-06-29 00:10:44 +0530

    working

--- Frontend_ICollab/src/components/Pages/MessagePage/ChatWindow/ChatWindow.jsx
+++ Frontend_ICollab/src/components/Pages/MessagePage/ChatWindow/ChatWindow.jsx
@@ -5,10 +5,13 @@ import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
 import KanbanBoard from "./KanbanBoard";
 import { getAblyInstance } from "../../../../utils/ablyClient";
+import { useSelector } from "react-redux";
 
 const ChatWindow = ({ chatData }) => {
   const [viewMode, setViewMode] = useState("chat");
   const [messages, setMessages] = useState([]);
+  const currentuser= useSelector((state) =>state.user.userData.name );
+
 
   useEffect(() => {
     const setupAbly = async () => {
@@ -25,7 +28,7 @@ const ChatWindow = ({ chatData }) => {
           message: msg.data.message,
           sender: msg.data.sender,
           timestamp: msg.data.timestamp,
-          isSender: msg.data.sender === chatData.name,
+          isSender: msg.data.sender === currentuser,
           id: msg.id,
         }));
         setMessages(historyMessages);
@@ -40,7 +43,7 @@ const ChatWindow = ({ chatData }) => {
             message: msg.data.message,
             sender: msg.data.sender,
             timestamp: msg.data.timestamp,
-            isSender: msg.data.sender === chatData.name,
+            isSender: msg.data.sender === currentuser,
             id: msg.id,
           },
         ]);
@@ -81,7 +84,7 @@ const ChatWindow = ({ chatData }) => {
       {viewMode === "chat" && (
         <>
           <MessageList messages={messages} isGroup={chatData.isGroup} />
-          <MessageInput channelId={chatData.channelId} senderName={chatData.name} />
+          <MessageInput channelId={chatData.channelId} senderName={currentuser} />
         </>
       )}
       {viewMode === "kanbanBoard" && <KanbanBoard />}
